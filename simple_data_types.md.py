d80fc3f117d5cb526d09f6cd06ed4c2a1821a1e6
RayTizocMartinez <raytizoc.martinez@gmail.com>
2016-08-08 15:38:05 -0500

    had to remind myself tinder is full of bots and scams

---
+++ simple_data_types.md.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+matches=20
+bots=3
+no_replies= matches-bots
+no_replies
+women_ive_faved=100
+time_spent_on_dates=None
+women_who_messeged_me['Pippa', 'Stanley', 'Claire']
+women_who_messeged_me=['Pippa', 'Stanley', 'Claire']
+women_who_messeged_me[0]
+women_who_messeged_me.pip
+women_who_messeged_me.pop
+del women_who_messeged_me[2]
+del women_who_messeged_me[12]
+del women_who_messeged_me[1]
+women_who_messeged_me
+get_ipython().magic('save simple_data_types.md 92-107')
