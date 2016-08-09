1ab6e32714cad01a44f99dee13ad52628c129a3a
RayTizocMartinez <raytizoc.martinez@gmail.com>
2016-08-09 13:36:21 -0500

    made a loop asking name, favortie color, and age.

---
+++ working_with_loops.py
@@ -0,0 +1,9 @@
+done = False
+
+while not done:
+    name = input('what your name is: ')
+    if not name:
+        done = True
+    else:
+        color = input("whats your favorite color: ")
+        age = input("how old are you: ")
